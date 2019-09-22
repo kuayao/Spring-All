5b6732b20a98f616a8346aa191d71b63b3c24192
1651705633@qq.com <zpc1998>
2019-09-22 18:23:04 +0800

    查询

--- groupthree/src/main/java/com/groupthree/GroupthreeApplication.java
+++ groupthree/src/main/java/com/groupthree/GroupthreeApplication.java
@@ -1,11 +1,13 @@
 package com.groupthree;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.groupthree.Controller")
+@MapperScan("com.groupthree.dao")
 public class GroupthreeApplication {
 
 	public static void main(String[] args) {
