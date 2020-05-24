a9be921041a88656d5cdbe278cb8207cb934d52c
kuayao <1651705633@qq.com>
2020-05-24 21:31:15 +0800

    微服务通信  OpenFeign  负载均衡，替换通信组件，通信日志，参数传递（GET/POST）

--- book-service-netfilix/src/main/java/com/springcloud/bookservicenetfilix/controller/BookController.java
+++ book-service-netfilix/src/main/java/com/springcloud/bookservicenetfilix/controller/BookController.java
@@ -2,9 +2,13 @@ package com.springcloud.bookservicenetfilix.controller;
 
 import com.springcloud.bookservicenetfilix.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author kuayao
@@ -25,5 +29,20 @@ public class BookController {
             book=new Book("3333","测试图书3",String.valueOf(request.getLocalPort()));
         }
         return book;
+    }
+
+
+    @GetMapping("/search")
+    public List<Book>  search(Book book){
+        List list = new ArrayList<Book>();
+        if (book.getSn().equals("1111") && book.getName().equals("x")){
+            list.add(new Book("1111","xxxx",""));
+        }
+        return  list;
+    }
+
+    @PostMapping("/create")
+    public  String createBook(@RequestBody Book book){
+        return book.getName()+"创建成功";
     }
 }
